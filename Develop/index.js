b43d86e319a470dc8fe2fa2052be03ab1717a5e2
Clay McKee <cmckee120993@gmail.com>
2022-08-29 18:15:06 -0700

    began inquirer form

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  { name: 'Title',
+  message: 'What is the title of your project?',
+  type: 'input' 
+},
+
+{ name: 'Desciption',
+message: 'Write a description for your project.',
+type: 'input'
+},
+
+{ name: 'Contents',
+message: 'Which of the following will be included in your README?',
+type: 'checkbox',
+choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Test Instructions']
+},
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
